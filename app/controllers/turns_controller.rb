f0144d4d43d7273f35438c82ffcb6edfffd49532
tamutomo223 <57092560+tamutomo223@users.noreply.github.com>
2019-11-24 16:31:21 +0900

    微調整

--- app/controllers/turns_controller.rb
+++ app/controllers/turns_controller.rb
@@ -1,24 +1,25 @@
 class TurnsController < ApplicationController
+
+  before_action :set_group, only: [:create,:result]
+  before_action :group_nil, only: [:new,:win,:lose]
+
   def new
+    #試合開始時の最初のターン作成
     @turn = Turn.new
-    @group = Group.find_by(id: params[:group_id])
-    if @group == nil
-      redirect_to root_path
-    else
-      @owner = @group.users[0]
-      @visiter = @group.users[1]
-      @turns = Turn.where(group_id: params[:group_id])
-      @turn_get = @turns[0]
-      if current_user.id == @visiter.id && @turns.length >= 1
-        redirect_to defences_new_path(@group,@turn_get.id,@turn_get.turn_num)
-      end
-    end  
+    @owner = @group.users[0]
+    @visiter = @group.users[1]
+
+    #グループidと一致しているターンがあるか確認
+    @turns = Turn.where(group_id: params[:group_id])
+    #そのターンの先頭を取得
+    @turn_get = @turns[0]
+    #
+    if current_user.id == @visiter.id && @turns.length >= 1
+      redirect_to defences_new_path(@group,@turn_get.id,@turn_get.turn_num)
+    end
   end
   
   def create
-    @group = Group.find(params[:group_id])
-    @owner = @group.users[0]
-    @visiter = @group.users[1]
     @turn = Turn.new(turn_params)
     if @turn.save && current_user.id == @owner.id
       redirect_to attacks_new_path(@group,@turn.id,@turn.turn_num)
@@ -28,23 +29,27 @@ class TurnsController < ApplicationController
   end
 
   def result
-    @group = Group.find(params[:group_id])
+    #事前準備
     @turn = Turn.find(params[:id])
-    @owner = @group.users[0]
-    @visiter = @group.users[1]
     @attacks = @turn.attack_id
     @defences = @turn.defence_id
     @turn_num = @turn.turn_num.to_i
-    
 
+    #攻撃、防御それぞれ対応レコードを取得
     @attack = Attack.find_by(id: @attacks)
     @defence = Defence.find_by(id: @defences)
 
+    #次のターンidを作成
     @next_turn = @turn.id.to_i + 1
 
+    #攻撃、防御それぞれのあげた指の数を計算
     @attackresult = @attack.right.to_i + @attack.left.to_i
     @defenceresult = @defence.right.to_i + @defence.left.to_i
+
+    #合わせた数を@totalに代入
     @total = @attackresult.to_i + @defenceresult.to_i
+
+    #オーナーかビジターのどっちが当たったか、外れたかで、urlに付与する番号を変える
     if @total == @attack.call.to_i && @attack.user == @owner
       @result = "あたり"
       @turn_num_next = @turn_num + 101
@@ -56,15 +61,17 @@ class TurnsController < ApplicationController
       @turn_num_next = @turn_num + 1
     end
 
+    #オーナーによって次のターンが生成されてるか確認
     @next_turn_made = Turn.find_by(group_id: @group.id,turn_num: @turn_num_next)
 
+    #ビジターはオーナーによって、次のターンが生成されていれば自動で攻撃または防御ページに移動する
     if @next_turn_made != nil && current_user.id.to_i == @attack.user_id.to_i && current_user.id == @visiter.id
       redirect_to defences_new_path(@group,@next_turn,@turn_num_next)
     elsif  @next_turn_made != nil && current_user.id.to_i == @defence.user_id.to_i && current_user.id == @visiter.id
       redirect_to attacks_new_path(@group,@next_turn,@turn_num_next)
-    end    
+    end
 
-    
+    #urlに付与された番号がどちらかの勝利条件に当てはまっていれば、それぞれ勝ちページと負けページへ飛ばす
     if @turn_num_next.to_i >= 200 && @turn_num_next.to_i <= 299 && current_user == @owner
       redirect_to turns_win_path(@group,1,@turn_num_next)
     elsif @turn_num_next.to_i >= 200 && @turn_num_next.to_i <= 299 && current_user == @visiter
@@ -78,47 +85,26 @@ class TurnsController < ApplicationController
     elsif @turn_num_next.to_i >= 1000 && current_user == @owner
       redirect_to turns_lose_path(@group,1,@turn_num_next)
     end
-
-    # @turns.each do |turn|
-    #   if turn.attack_id != nil
-    #     @turn_attack_num = turn.id
-    #     @attacks = turn.attack_id.to_i
-    #     @turn_num = turn.turn_num.to_i
-    #   end  
-    # end
-    # @turns.each do |turn|
-
-    #   if turn.defence != nil
-    #     @turn_defence_num = turn.id
-    #     @defences = turn.defence_id.to_i
-    #     @turn_num = turn.turn_num.to_i
-    #   end  
-    # end
-    
-    
-
-    # @nil_turns = Turn.where(attack_id: nil).where(defence_id: nil)
-    # if @nil_turns.length >= 1
-    #   @nil_turns.each do |turn|
-    #   turn.destroy
-    #   end  
-    # end  
-    
   end
   
   def win
-    @group = Group.find_by(id: params[:group_id])
-    if @group == nil
-      redirect_to root_path
-    end
   end
   
   def lose
+  end
+
+  def set_group 
+    @group = Group.find(params[:group_id])
+    @owner = @group.users[0]
+    @visiter = @group.users[1]
+  end
+
+  def group_nil
     @group = Group.find_by(id: params[:group_id])
     if @group == nil
       redirect_to root_path
     end
-  end
+  end  
 
   private
 
